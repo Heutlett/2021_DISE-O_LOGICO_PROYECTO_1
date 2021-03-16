e1333f3c86066f82a1c7d5745cbb88e0d9f2af5e
Shakime <jeykimers.johnson@gmail.com>
2021-03-16 11:25:06 -0600

    [InProgress] Tabla 2 falta verificar datos.

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -12,13 +12,17 @@ tablesColor ='lightgray' #azul mas claro
 #Text
 font = "Century Gothic"
 label1 = 'Ingrese un número binario:'
+verifyTxt= 'El error se encuentra en el bit: '
 
 #Variables
 parity = 'Par'
 
-headers1 = (
-    ' ', 'p1', 'p2', 'd1', 'p3', 'd2', 'd3', 'd4', 'p4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'd11', 'p5', 'd12')
-description1 = ["Palabra de datos (sin paridad):", "p1", "p2", "p3", "p4", "p5", "Palabra de datos (con paridad):"]
+headers1 = (' ', 'p1', 'p2', 'd1', 'p3', 'd2', 'd3', 'd4', 'p4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'd11', 'p5', 'd12')
+description1 = ["Sin paridad:", "p1", "p2", "p3", "p4", "p5", "Con paridad:"]
+
+headers2 = (' ', 'p1', 'p2', 'd1', 'p3', 'd2', 'd3', 'd4', 'p4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'd11', 'p5', 'd12', 'Prueba', 'Bit')
+description2 = ["Palabra:", "p1", "p2", "p3", "p4", "p5"]
 
 rows1 = []
 rows2 = []
+entrysList = []
